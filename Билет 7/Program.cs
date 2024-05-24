abb78327384b4db6b60dc66498270e48835219c6
user <user@DESKTOP-QEASJ0T>
2024-05-24 10:15:05 +0300

    Седьмой билет

--- Билет 7/Program.cs
+++ Билет 7/Program.cs
@@ -12,7 +12,19 @@ namespace Билет_7
         {
             while (true)
             {
+                Console.Clear();
+                int A = 5;
+                int K = 15;
+                double H = 0.2;
+                double S = 0;
+                int i = 1;
 
+                for (; i <= K; i++)
+                    S = S + (A+H*H)*i/(i+1);
+
+                Console.WriteLine($"Y равен {H/(8*S)}");
+                Console.WriteLine("Введите любую кнопку для рестарта");
+                Console.ReadKey();
             }
         }
     }
